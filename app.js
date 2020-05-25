991a403422653129f42513d0bcc92a0e1834cae9
Roy Harrow <roy.harrow@uk.ibm.com>
2020-05-25 19:04:19 +0100

    Added comments

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+//Comments
 console.log("Good evening to all of our viewers.\n")
+//More Comments
 console.log("Hello Everyone In the World from Roy Harrow\n");
+//More Comments
 console.log("And it's goodnight from me and him.\n")
