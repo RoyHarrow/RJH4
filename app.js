3f1bc0c6fc484a0a00c202e089c9865c5ab1ee6f
Roy Harrow <roy.harrow@uk.ibm.com>
2020-05-25 19:39:28 +0100

    Added comments at the very end (minus "Le Fin")

--- app.js
+++ app.js
@@ -4,3 +4,5 @@ console.log("Good evening to all of our viewers.\n")
 console.log("Hello Everyone In the World from Roy Harrow\n");
 //More Comments
 console.log("And it's goodnight from me and him.\n")
+//End of File Comment
+//The End
